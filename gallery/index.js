8556d26f869a97ddb5cf34eface4d7144042cea1
jin kyung lee <jk@jinui-MacBook-Pro.local>
2019-03-05 23:05:49 +0900

    add gallery

---
+++ gallery/index.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function() {
+	jQuery('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+		},
+    });
+});
