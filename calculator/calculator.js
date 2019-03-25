80adf811a956163440713626290d673db47563bb
jin kyung lee <jk@jinui-MacBook-Pro.local>
2019-03-25 23:24:59 +0900

    계산기 수정

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -46,3 +46,28 @@ function back(){
     var txt_board = document.getElementsByClassName("txt_board")[0];
     txt_board.value = txt_board.value.substring(0, txt_board.value.length-1);
 }
+
+
+
+
+var calculator = {
+    plus : function(a, b) {
+        return a+b;
+    },
+    sub :function(a, b) {
+        return a-b;
+    },
+    mul :function(a, b) {
+        return a*b;
+    },
+    divi :function(a, b) {
+        return a / b;
+    }
+
+}
+
+
+console.log( calculator.plus(1,1) );
+console.log( calculator.sub(1,1) );
+console.log( calculator.mul(10,2) );
+console.log( calculator.divi(10,2) );
