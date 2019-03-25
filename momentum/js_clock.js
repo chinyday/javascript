0faea92bdd7e75ae2b2706b48a1e60bb034ae462
jin kyung lee <jk@jinui-MacBook-Pro.local>
2019-03-26 00:15:06 +0900

    모멘텀 추가

---
+++ momentum/js_clock.js
@@ -0,0 +1,18 @@
+const clockContainer = document.querySelector(".js-clock"),
+    clockTitle = clockContainer.querySelector("h2"); 
+
+function getTime() {
+    const date = new Date();
+    const hour = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    clockTitle.innerText = `${
+        hour < 10 ? `0${hour}` : hour} : ${
+            minutes < 10 ? `0${minutes}` : minutes}`
+}
+
+function init() {
+    getTime(); 
+    setInterval(getTime, 1000);
+}
+init();
