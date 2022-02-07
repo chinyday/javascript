3d2c8dbfc0b679d54af01111e8cdb558e5089044
이진경 <chinyday@gmail.com>
2022-02-07 19:49:20 +0900

    그림판 js 코드 리팩토링 - 이벤트 위임 활용하여 코드 정리 - 짧은 코드는 화살표 함수로 변경

--- paintingImg/index.js
+++ paintingImg/index.js
@@ -1,102 +1,94 @@
-const canvas = document.getElementById('jsCanvas');
-const ctx =  canvas.getContext('2d');
+let canvas = document.getElementById('jsCanvas');
+let ctx = canvas.getContext('2d');
 let colors = document.getElementsByClassName('color_btn');
 let range = document.getElementById('jsRange');
-let fill = document.getElementById('jsFill');
-let saveBtn = document.getElementById('jsSave');
+let btn_wrap = document.querySelector('.btn_wrap');
 
 ctx.width = 600;
 ctx.height = 600;
-
 ctx.fillStyle = "#fff";
-ctx.fillRect(0, 0, ctx.width, ctx.height); 
-
 ctx.strokeStyle = "#2d3436";
 ctx.fillStyle = "#2d3436";
 ctx.lineWidth = 2.5;
+ctx.fillRect(0, 0, ctx.width, ctx.height);
 
-let painting = false;
-let filling = false;                    
+let controll = {
+	painting : false,
+	filling : false
+}
+
+function startPainting() {
+	controll.painting = true;
+}
+
+function stopPainting() {
+	controll.painting = false;
+}
 
 function onMouseMove(event) {
-    let x = event.offsetX;
-    let y = event.offsetY;
+	let x = event.offsetX;
+	let y = event.offsetY;
 
-    if(!painting){
-        ctx.beginPath();
-        ctx.moveTo(x, y);
-    }else{
-        ctx.lineTo(x, y);
-        ctx.stroke();
-    }
+	if (!controll.painting) {
+		ctx.beginPath();
+		ctx.moveTo(x, y);
+	} else {
+		ctx.lineTo(x, y);
+		ctx.stroke();
+	}
 }
 
-function startPainting(event) {
-    painting = true;
-}
-
-function stopPainting(event) {
-    painting = false;
-}
-
-function handleChangeColor(event) {
-    ctx.strokeStyle = event.target.style.backgroundColor;
-    ctx.fillStyle = event.target.style.backgroundColor;
-}
-
-function handleChangeRange(event) {
-    range.setAttribute('value', event.target.value);
-    ctx.lineWidth = event.target.value;
-}
-
-function handleFillClick() {
-    if(filling === true){
-        filling = false;
-        fill.innerText = "fill";
-    }else{
-        filling = true;
-        fill.innerText = "paint";
-    }
-}
-
-function handleCanvasClick() {
-    if(filling){
-        ctx.fillRect(0, 0, ctx.width, ctx.height); 
-    }
-}
-
-function handleClickCM(event) {
-    event.preventDefault();
+function handleFillClick(event) {
+	if (controll.filling === true) {
+		controll.filling = false;
+		event.innerText = "fill";
+	} else {
+		controll.filling = true;
+		event.innerText = "paint";
+	}
 }
 
 function handleSaveImg() {
-    let image = canvas.toDataURL('image/png');
-    let link = document.createElement('a');
-
-    link.href = image;
-    link.download = 'image';
-    link.click();
+	let image = canvas.toDataURL('image/png');
+	let link = document.createElement('a');
+	link.href = image;
+	link.download = 'image';
+	link.click();
 }
 
-if(canvas){
-    canvas.addEventListener('mousemove', onMouseMove);
-    canvas.addEventListener('mouseup', stopPainting);
-    canvas.addEventListener('mousedown', startPainting);
-    canvas.addEventListener('mouseleave', stopPainting);
-    canvas.addEventListener('click', handleCanvasClick);
-    canvas.addEventListener('contextmenu', handleClickCM);
+function handleCanvasClick() {
+	if (controll.filling) {
+		ctx.fillRect(0, 0, ctx.width, ctx.height);
+	}
 }
- 
-Array.from(colors).forEach(function(color) {
-    color.addEventListener('click', handleChangeColor);
+
+if (canvas) {
+	canvas.addEventListener('mousemove', onMouseMove);
+	canvas.addEventListener('mouseup', stopPainting);
+	canvas.addEventListener('mousedown', startPainting);
+	canvas.addEventListener('mouseleave', stopPainting);
+	canvas.addEventListener('click', handleCanvasClick);
+	canvas.addEventListener('contextmenu', event => event.preventDefault() );
+}
+
+Array.from(colors).forEach(function (color) {
+	color.addEventListener('click', (event) => {
+		ctx.strokeStyle = event.target.style.backgroundColor;
+		ctx.fillStyle = event.target.style.backgroundColor;
+	});
 });
 
-if(range){
-    range.addEventListener('input', handleChangeRange);
-}
-if(fill){
-    fill.addEventListener('click', handleFillClick);
-}
-if(saveBtn){
-    saveBtn.addEventListener('click', handleSaveImg);
-}
+range.addEventListener('input', (event) => {
+	range.setAttribute('value', event.target.value);
+	ctx.lineWidth = event.target.value;
+});
+
+btn_wrap.addEventListener('click', (event) => {
+	let target = event.target;
+
+	if(target.id === "jsFill"){
+		handleFillClick(target);
+	}else if(target.id === "jsSave"){
+		handleSaveImg();
+	}
+});
