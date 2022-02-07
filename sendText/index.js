6378f14f0e40ec54b51c02a3f2ab3d69891faee4
이진경 <chinyday@gmail.com>
2022-02-07 18:38:37 +0900

    정렬 작업

--- sendText/index.js
+++ sendText/index.js
@@ -1,117 +1,111 @@
+let categoryWrap = document.querySelector('.category_wrap');
+let AllradioBtn = categoryWrap.getElementsByTagName('input');
+let sendBtn = document.querySelector('.send_btn');
+let txtArea = document.querySelector('.txt_area');
+let list = document.querySelector('.js-list');
 
+const LIST_LOCAL = 'sendTxt';
+let listArr = [];
 
-    
-    let categoryWrap = document.querySelector('.category_wrap');
-    let AllradioBtn = categoryWrap.getElementsByTagName('input');
-    let sendBtn = document.querySelector('.send_btn');
-    let txtArea = document.querySelector('.txt_area');
-    let list = document.querySelector('.js-list');
+let listInLocal = localStorage.getItem(LIST_LOCAL);
+let loadListInLacal = JSON.parse(listInLocal);
 
-    const LIST_LOCAL = 'sendTxt';
-    let listArr = [];
+// 로컬에 데이터 저장
+function saveListLocal() {
+  localStorage.setItem(LIST_LOCAL, JSON.stringify(listArr));
+}
 
-    let listInLocal = localStorage.getItem(LIST_LOCAL);
-    let loadListInLacal = JSON.parse(listInLocal);
+// li 출력
+let all_tr = "";
+function paintTR(data) {
 
+  document.querySelector('.subtitle').classList.remove('off');
 
-    // 로컬에 데이터 저장
-    function saveListLocal() {
-        localStorage.setItem(LIST_LOCAL,JSON.stringify(listArr));
+  let tr = "";
+  tr = ` <tr>
+      <td class="to">${data.radioBtn}</td>
+      <td><pre>${data.txt}</pre></td>
+    </tr>`
+  all_tr += tr;
+  list.innerHTML = all_tr;
+
+  let childNodes = list.childNodes,
+      i = childNodes.length;
+
+  while (i--) list.appendChild(childNodes[i]);
+}
+
+//  list를 출력하고, 로컬에 데이터를 저장
+function paintList(text) {
+  paintTR(text);
+  listArr.push(text);
+  saveListLocal(); // 데이터 저장
+}
+
+// 라디오버튼을 클릭했을 때 카테고리별로 정렬
+function sortList(data) {
+  all_tr = "";
+  list.innerHTML = "";
+  for (let i = 0; i < data.length; i++) {
+    paintTR(data[i]);
+  }
+}
+
+// send 버튼을 눌렀을 때 발생
+function handleSumit(event) {
+  let radioBtnValue = '';
+  for (let i = 0; i < AllradioBtn.length; i++) {
+    if (AllradioBtn[i].checked == true) {
+      radioBtnValue = AllradioBtn[i].value;
     }
+  }
 
-    // li 출력
-    let all_tr = "";
-    function paintTR(data) {
-        document.querySelector('.subtitle').classList.remove('off');
+  let currValueObj = {
+    radioBtn: radioBtnValue,
+    txt: txtArea.value,
+  }
 
-        let tr = "";
-        tr = ` <tr>
-                <td class="to">${data.radioBtn}</td>
-                <td><pre>${data.txt}</pre></td>
-            </tr>`
-        all_tr += tr; 
-        list.innerHTML = all_tr;
-       
-        let childNodes = list.childNodes,
-        i = childNodes.length;
+  if (txtArea.value != "" && radioBtnValue != "") {
+    paintList(currValueObj);
+  }
+  txtArea.value = '';
+}
 
-        while (i--)
-            list.appendChild(childNodes[i]);  
-    }
+//선택된 버튼에 checked 설정
+function checkedRadioBtn(event) {
+  let clickedBtn = event.target;
+  let sameList = [];
 
-    //  list를 출력하고, 로컬에 데이터를 저장
-    function paintList(text) {
-        paintTR(text);
-        listArr.push(text);
-        saveListLocal(); // 데이터 저장
-    }
+  if (listInLocal == null){
+    return;
+  }
+  // if (listInLocal !== null) {
+    loadListInLacal.forEach(function (listArr) {
+      if (clickedBtn.value == listArr.radioBtn) {
+        sameList.push(listArr);
+      }
+    });
+  // }
+  sortList(sameList);
+}
 
-    // 라디오버튼을 클릭했을 때 카테고리별로 정렬
-    function sortList(data) { 
-        all_tr = "";   
-        list.innerHTML = "";
-        for (let i = 0; i < data.length; i++) {
-            paintTR(data[i]);
-        }
-    }
+//만약 로컬에 저정되어 있는 정보가 있을 때 출력
+function loadList() {
+  if (listInLocal !== null) {
+    loadListInLacal.forEach(function (listArr) {
+      paintList(listArr);
+    });
+  } else {
+    document.querySelector('.subtitle').classList.add('off');
+  }
+}
 
-    // send 버튼을 눌렀을 때 발생
-    function handleSumit(event) { 
-        let radioBtnValue = '';
-        for (let i = 0; i < AllradioBtn.length; i++) {  
-            if(AllradioBtn[i].checked == true){
-                radioBtnValue = AllradioBtn[i].value;
-            } 
-        }
+function init() {
+  loadList();
+  for (let i of AllradioBtn) {
+    i.addEventListener('click', checkedRadioBtn);
+  }
+}
 
-        let currValueObj = {
-            radioBtn : radioBtnValue,
-            txt : txtArea.value,
-        } 
-
-        if (txtArea.value != "" && radioBtnValue != "") {   
-            paintList(currValueObj);
-        }
-        txtArea.value = '';
-    }
-
-    //선택된 버튼에 checked 설정
-    function checkedRadioBtn(event) {
-        let clickedBtn = event.target;
-        let sameList = []; 
-
-        // jQuery(document).ready(function() {
-        //     jQuery(clickedBtn).attr('checked',true).siblings().removeAttr('checked');
-        // });
-        
-        if(listInLocal !== null){
-            loadListInLacal.forEach(function(listArr){
-                if(clickedBtn.value == listArr.radioBtn){
-                    sameList.push(listArr);   
-                }
-            });      
-        }
-        sortList(sameList);
-    }
-
-    //만약 로컬에 저정되어 있는 정보가 있을 때 출력
-    function loadList() {
-        if(listInLocal !== null){
-            loadListInLacal.forEach(function(listArr){
-                paintList(listArr);
-            });   
-        }else{
-           document.querySelector('.subtitle').classList.add('off');
-        }
-    }
-
-    sendBtn.addEventListener('click', handleSumit); 
-
-    function init() {
-        loadList();
-        for (let i of AllradioBtn) {
-            i.addEventListener('click',checkedRadioBtn);   
-        }
-    }
-    init();
-
+init();
+sendBtn.addEventListener('click', handleSumit);
