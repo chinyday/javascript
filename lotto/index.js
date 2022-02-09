63a8c4004e237d0fb486d06017ed0166da578d61
이진경 <chinyday@gmail.com>
2022-02-09 15:48:52 +0900

    로또 추첨기 코드 개선

--- lotto/index.js
+++ lotto/index.js
@@ -1,66 +1,58 @@
-var lotto_arr = new Array(6); //로또를 담을 배열
-var lotto_num;// = Math.floor(Math.random()*46 )+1; //1~466까지 랜덤으로 숫자 뽑기 
-var count;
+const LOTTONUMBER = 7;
+let lotto_arr = []; //로또를 담을 배열
 
-
-//로또 담기
-function setLottoNumber(count){
-    lotto_arr[count] = lotto_num;
-}
-
-//숫자 랜덤으로 받기
-function randomNumber() {
-    lotto_num = Math.floor(Math.random()*46 )+1; //배열에 넣기
-    return lotto_num;
-}
-
-//배열에 동일한 값이 있는지 확인
-function checkExist(lotto_num) {  
-    if(lotto_arr.indexOf(lotto_num) != '-1'){
-        lotto_num = randomNumber();
-    }
-    setLottoNumber(count);
-}
-
-// 
 function startLotto() {
-    for (let i = 0; i < lotto_arr.length; i++) {
-        count = i;
-        lotto_num = randomNumber(); 
-        checkExist(lotto_num);  
+    let lotto_num;
+    lotto_arr = [];
+    for (let i = 0; i < LOTTONUMBER; i++) {
+        lotto_num = randomNumber();
+        lotto_arr[i] = lotto_num;
     }
+    setLottoUI();
+}
+
+function randomNumber() {
+    let num;
+    let flg = true;
+    while (flg) {
+        num = Math.floor(Math.random() * 46) + 1; 
+        if (lotto_arr.indexOf(num) == '-1') {       
+            flg = false;
+        }
+    }
+    return num;
 }
 
 // 로또 당첨 숫자 화면에 출력
 function setLottoUI() {
-    // 오름 차순으로 숫자 정렬
-    lotto_arr.sort(function(a, b) { 
-        return a - b;
-    });
+    let ul_wrap = document.getElementById('today_lotto');
+    let ul = document.createElement('ul');
+    let li;
 
-    var ul_wrap = document.getElementById('today_lotto');
-    var ul ='<ul>';
-    var point_color;
-    for (let i = 0; i < lotto_arr.length; i++) {
-        if (lotto_arr[i] <=10) {
-            point_color = 'num10';
-        }else if (10 < lotto_arr[i] && lotto_arr[i] <= 20) {
-            point_color = 'num20';
-        }else if (20 < lotto_arr[i] && lotto_arr[i] <= 30) {
-            point_color = 'num30';
-        }else if (30 < lotto_arr[i] && lotto_arr[i] <= 40) {
-            point_color = 'num40';
-        }else if (40 < lotto_arr[i]) {
-            point_color = 'num50';
-        }
-        ul += "<li class="+ point_color +" >" + lotto_arr[i] + "</li>";  
+    if(ul_wrap.childNodes.length > 0){
+        ul_wrap.innerHTML = '';
     }
-    ul += '</ul>' 
-    ul_wrap.innerHTML = ul;
+
+    for (let i = 0; i < lotto_arr.length; i++) {
+        li = paintLi(lotto_arr[i]);
+        ul.insertAdjacentHTML('beforeend', li);
+    }
+
+    ul_wrap.appendChild(ul);
 }
 
-function btn_lotto() {
-    startLotto();
-    setLottoUI();
+function paintLi(num) {
+    let point_color;
+
+    if (num <= 10) { point_color = 'num10'; }
+    else if (10 < num && num <= 20) { point_color = 'num20'; }
+    else if (20 < num && num <= 30) { point_color = 'num30'; }
+    else if (30 < num && num <= 40) { point_color = 'num40'; }
+    else if (40 < num) { point_color = 'num50'; }
+
+    return `<li class="${point_color}">${num}</li>`;
 }
-  
+
+function btn_lotto() {   
+    startLotto();
+}
